9c6ef632467b9992828292a48d8f29f5f387ebf5
Annie Wiegel <annewiegel17@gmail.com>
2021-01-20 03:50:11 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns 
+end
